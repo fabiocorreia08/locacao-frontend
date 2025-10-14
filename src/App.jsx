037fab1ea838971ba255fa47a84cef49fa315f9d
Fabio Correia <fabiocorreia08@gmail.com>
2025-10-14 19:13:10 -0300

    URL FRONTEND

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react';
 import AppRoutes from './routes/AppRoutes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import SessionExpiredPopup from './components/SessionExpired/SessionExpiredPopup';
 
 function App() {
-  useEffect(() => {
-    setTimeout(() => {
-      window.dispatchEvent(new Event('session-expired'));
-    }, 3000); // dispara após 3 segundos
-  }, []);
-
+  
   return (
     <div className="app-container">
       <AppRoutes />
