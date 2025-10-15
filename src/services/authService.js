1fddc277a17daed2aec7ae37606fad4521a7bd5a
Fabio Correia <fabiocorreia08@gmail.com>
2025-10-14 23:42:50 -0300

    configuracao cors

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,5 +3,6 @@ import axios from 'axios';
 const API_URL = `${process.env.REACT_APP_API_URL}/api/auth`;
 
 export const login = (credentials) => {
+  console.log(API_URL);
   return axios.post(`${API_URL}/login`, credentials);
 };
