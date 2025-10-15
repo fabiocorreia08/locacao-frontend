e16c9e408ceeb3f60416fb753efe3ab52131bda3
Fabio Correia <fabiocorreia08@gmail.com>
2025-10-15 00:04:02 -0300

    backend

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const API_URL = `${process.env.REACT_APP_API_URL}/api/auth`;
 
-export const login = (credentials) => {
-  console.log(API_URL);
+export const login = (credentials) => { 
   return axios.post(`${API_URL}/login`, credentials);
 };
